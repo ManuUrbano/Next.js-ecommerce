3e7180c8c6b1e2b7b68194152293bab584006558
ManuUrbano <jmanuurbano@gmail.com>
2021-06-20 18:43:13 +0200

    deploy

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_PATH = "http://localhost:1337";
+export const BASE_PATH = "https://textilesurbano.herokuapp.com";
 export const TOKEN = "token";
 export const CART = "cart";
 export const STRIPE_TOKEN = "pk_test_51J2KASB2kGuty4FgejZrXteA8k1fRvpB2zpbuVtOvnNcLVP2MglPzf9pYsEaT7zZzET7uSAuern31DQ4Zqa0aPP0002gKBoHGt";
